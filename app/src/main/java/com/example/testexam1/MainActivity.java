e83811714ec137697dbb2dfab35beaeb178ec6b4
alimirjavadi <sydlymyrjwady@gmail.com>
2026-06-11 20:32:06 +0330

    Recyclerview and Alert Dialoog

--- app/src/main/java/com/example/testexam1/MainActivity.java
+++ app/src/main/java/com/example/testexam1/MainActivity.java
@@ -20,4 +20,5 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapterName);
         recyclerView.setLayoutManager(layoutManager);
     }
+
 }
--- app/src/main/java/com/example/testexam1/NameViewHolder.java
+++ app/src/main/java/com/example/testexam1/NameViewHolder.java
@@ -1,5 +1,7 @@
 package com.example.testexam1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,18 +14,40 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
 
     private ImageView image_item ;
     private TextView textView;
+    private TextView moretext;
     public NameViewHolder(@NonNull View itemView) {
         super(itemView);
         image_item = itemView.findViewById(R.id.img_main);
         textView = itemView.findViewById(R.id.tv_main);
+        moretext = itemView.findViewById(R.id.tvmore);
     }
     public void setitems(String name , int imageid){
         textView.setText(name);
         image_item.setImageResource(imageid);
-        image_item.setOnClickListener(new View.OnClickListener() {
+
+        moretext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),name,Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                AlertDialog.Builder alertdialog = new AlertDialog.Builder(view.getContext());
+                alertdialog.setTitle("some title from android");
+                alertdialog.setMessage("some message from android");
+
+                alertdialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                alertdialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                alertdialog.setCancelable(false);
+                alertdialog.show();
             }
         });
     }
