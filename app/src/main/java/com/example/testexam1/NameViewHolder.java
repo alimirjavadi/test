d05f5608daf9c3c5bca1b7977ee2a977208a284b
alimirjavadi <sydlymyrjwady@gmail.com>
2026-06-11 22:01:47 +0330

    RecyclerView and TimePikerDialog

--- app/src/main/java/com/example/testexam1/NameViewHolder.java
+++ app/src/main/java/com/example/testexam1/NameViewHolder.java
@@ -16,44 +16,17 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
 
     private ImageView image_item ;
     private TextView textView;
-    private TextView moretext;
     private TextView tvtime;
     public NameViewHolder(@NonNull View itemView) {
         super(itemView);
         image_item = itemView.findViewById(R.id.img_main);
         textView = itemView.findViewById(R.id.tv_main);
-        moretext = itemView.findViewById(R.id.tvmore);
         tvtime = itemView.findViewById(R.id.tvtime);
     }
     public void setitems(String name , int imageid){
         textView.setText(name);
         image_item.setImageResource(imageid);
 
-        moretext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alertdialog = new AlertDialog.Builder(view.getContext());
-                alertdialog.setTitle("some title from android");
-                alertdialog.setMessage("some message from android");
-
-                alertdialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-
-                alertdialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-
-                alertdialog.setCancelable(false);
-                alertdialog.show();
-            }
-        });
 
         tvtime.setOnClickListener(new View.OnClickListener() {
             @Override
