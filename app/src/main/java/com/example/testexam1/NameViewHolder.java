8182891c43f19539fd1b019c5de0805086ca0606
alimirjavadi <sydlymyrjwady@gmail.com>
2026-06-11 21:52:00 +0330

    Recyclerview and alertdialog with TimePickerDialog

--- app/src/main/java/com/example/testexam1/NameViewHolder.java
+++ app/src/main/java/com/example/testexam1/NameViewHolder.java
@@ -1,10 +1,12 @@
 package com.example.testexam1;
 
 import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,11 +17,13 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
     private ImageView image_item ;
     private TextView textView;
     private TextView moretext;
+    private TextView tvtime;
     public NameViewHolder(@NonNull View itemView) {
         super(itemView);
         image_item = itemView.findViewById(R.id.img_main);
         textView = itemView.findViewById(R.id.tv_main);
         moretext = itemView.findViewById(R.id.tvmore);
+        tvtime = itemView.findViewById(R.id.tvtime);
     }
     public void setitems(String name , int imageid){
         textView.setText(name);
@@ -48,6 +52,19 @@ public class NameViewHolder extends RecyclerView.ViewHolder {
 
                 alertdialog.setCancelable(false);
                 alertdialog.show();
+            }
+        });
+
+        tvtime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(view.getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourofday, int minute) {
+
+                    }
+                },23,01,true);
+                timePickerDialog.show();
             }
         });
     }
