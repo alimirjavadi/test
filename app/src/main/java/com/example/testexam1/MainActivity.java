c496d01f3c56f25f1cbc349b5136c2013eeeb22c
alimirjavadi <sydlymyrjwady@gmail.com>
2026-06-12 15:54:05 +0330

    Permission

--- app/src/main/java/com/example/testexam1/MainActivity.java
+++ app/src/main/java/com/example/testexam1/MainActivity.java
@@ -1,27 +1,47 @@
 package com.example.testexam1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.app.ProgressDialog;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+            }else {
+                Toast.makeText(this, "Permision is not granted ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.rv_main);
-        AdapterName adapterName = new AdapterName();
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        recyclerView.setAdapter(adapterName);
-        recyclerView.setLayoutManager(layoutManager);
+        String permision;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permision = Manifest.permission.READ_MEDIA_AUDIO;
+        }else{
+            permision = Manifest.permission.READ_EXTERNAL_STORAGE;
+        }
 
+        if (ContextCompat.checkSelfPermission(this,permision)== PackageManager.PERMISSION_GRANTED){
+
+        }else {
+            ActivityCompat.requestPermissions(this , new String[]{permision},100);
+        }
     }
 
 }
