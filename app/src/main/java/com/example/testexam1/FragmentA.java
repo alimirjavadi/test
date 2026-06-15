294300f843abca1a527e5a6dcae60744d3f3b8f1
alimirjavadi <sydlymyrjwady@gmail.com>
2026-06-15 22:15:49 +0330

    ViewPager with Fragment

--- app/src/main/java/com/example/testexam1/FragmentA.java
+++ app/src/main/java/com/example/testexam1/FragmentA.java
@@ -10,28 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentB extends Fragment {
-
-    public static final String KEY_NAME ="STRING";
+public class FragmentA extends Fragment {
+    private static final String KEY_DATA = "SOME_KEY";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_b, container, false);
-    }
+        return inflater.inflate(R.layout.fragment_a,container,false);
 
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView textView = view.findViewById(R.id.textview);
-        textView.setText(getArguments().getString(KEY_NAME));
+        TextView fragment_textview = view.findViewById(R.id.fragment_textview);
+        fragment_textview.setText(getArguments().getString(KEY_DATA));
     }
 
-    public static FragmentMain newInstance(String data) {
+    public static FragmentA newInstance(String data) {
 
         Bundle args = new Bundle();
-        args.putString(KEY_NAME,data);
-        FragmentMain fragment = new FragmentMain();
+        args.putString(KEY_DATA,data);
+        FragmentA fragment = new FragmentA();
         fragment.setArguments(args);
         return fragment;
     }
--- app/src/main/java/com/example/testexam1/MainActivity.java
+++ app/src/main/java/com/example/testexam1/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.testexam1;
 
-import static com.example.testexam1.FragmentB.KEY_NAME;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,22 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager2 viewPager2 = findViewById(R.id.viewpager2);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
 
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Bundle bundle = new Bundle();
-                bundle.putString(KEY_NAME , "some text From Activity Main");
-                FragmentB fragmentB = new FragmentB();
-                fragmentB.setArguments(bundle);
-
-                FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction2.replace(R.id.fragmentContainerView, fragmentB);
-                fragmentTransaction2.addToBackStack(null);
-                fragmentTransaction2.commit();
-            }
-        });
-
+        viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        viewPager2.setAdapter(viewPagerAdapter);
     }
 }
