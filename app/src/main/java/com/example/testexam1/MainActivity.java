b017ce8de204a7a9148e3ed597cc2eab517bfdc3
alimirjavadi <sydlymyrjwady@gmail.com>
2026-06-17 23:27:29 +0330

    Android Basic Project

--- app/src/main/java/com/example/testexam1/MainActivity.java
+++ app/src/main/java/com/example/testexam1/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.testexam1;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
@@ -12,11 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ViewPager2 viewPager2 = findViewById(R.id.viewpager2);
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
-
-        viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
-        viewPager2.setAdapter(viewPagerAdapter);
     }
 }
